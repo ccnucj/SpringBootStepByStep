2ee0c1f6eb19ff3e6446e236812f55dfd6bde08b
陈杰 <chenj@chinatelecom.cn>
2017-07-18 16:04:40 +0800

    spring 资源值注入的各种方式

--- highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/el/DemoService.java
+++ highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/el/DemoService.java
@@ -3,6 +3,10 @@ package com.wisely.highlight_spring4.ch2.el;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+/**
+ * 将一个值保存在一个bean中
+ */
+
 @Service
 public class DemoService {
 	@Value("another value") //1
--- highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/el/ElConfig.java
+++ highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/el/ElConfig.java
@@ -11,32 +11,53 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 
+/**
+ * spring 具备的各种资源属性的注入
+ */
 @Configuration
 @ComponentScan("com.wisely.highlight_spring4.ch2.el")
 @PropertySource("classpath:test.properties")//7
 public class ElConfig {
 
+    //直接用字符串赋值
     @Value("I Love You!") //1
     private String normal;
 
+    //读取操作系统的属性值
     @Value("#{systemProperties['os.name']}") //2
     private String osName;
 
+    //计算表达式的值
     @Value("#{ T(java.lang.Math).random() * 100.0 }") //3
     private double randomNumber;
 
+    //引入bean的属性值
     @Value("#{demoService.another}") //4
     private String fromAnother;
 
+    //加载文件，保存在Resource中
     @Value("classpath:test.txt") //5
     private Resource testFile;
 
     @Value("http://www.baidu.com") //6
     private Resource testUrl;
 
+    /**
+     * 直接读取配置文件的属性值
+     * 这样生效的前提在于使用
+     * （1）@PropertySource("classpath:test.properties")
+     * （2） @Bean //7
+     *       public static PropertySourcesPlaceholderConfigurer propertyConfigure() {
+     *       return new PropertySourcesPlaceholderConfigurer();
+     *       }
+     */
     @Value("${book.name}") //7
     private String bookName;
 
+    /**
+     * 将配置文件的属性值，直接注入到一个Environment中
+     * 后续直接读取 如下：environment.getProperty("book.author")
+     */
     @Autowired
     private Environment environment; //7
 
