33aa8bf9060d90cd1ffa04b0e2364548e88374ca
陈杰 <chenj@chinatelecom.cn>
2017-07-18 16:19:36 +0800

    profile 灵活选择配置

--- highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/ProfileConfig.java
+++ highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/ProfileConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+/**
+ * 类似配置可以扩展以满足各种场景下的需求
+ */
 @Configuration
 public class ProfileConfig {
 	@Bean
