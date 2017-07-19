51df938d19f91f60eb7f6c85c8c08d7e4e610aa7
陈杰 <chenj@chinatelecom.cn>
2017-07-19 09:34:57 +0800

    组合注解
    组合注解的值会覆盖元注解的值

--- highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch3/annotation/WiselyConfiguration.java
+++ highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch3/annotation/WiselyConfiguration.java
@@ -1,21 +1,22 @@
 package com.wisely.highlight_spring4.ch3.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.annotation.*;
+
+/**
+ * 组合注解
+ * 组合后的注解会覆盖元注解的值
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Configuration //1
 @ComponentScan //2
 public @interface WiselyConfiguration {
-	
+
+	// 覆盖@ComponentScan value参数
 	String[] value() default {}; //3
 
 }
