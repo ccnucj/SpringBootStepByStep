4331b39ee2ae66531cfd8ebd3401d1d5b4f9195c
陈杰 <chenj@chinatelecom.cn>
2017-07-20 10:02:55 +0800

    sse
    服务器端推送事件server-send-event
    有两点需要配置
    （1）指定produces="text/event-stream"
    （2）js上代码

--- highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_5/SseController.java
+++ highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_5/SseController.java
@@ -1,11 +1,18 @@
 package com.wisely.highlight_springmvc4.web.ch4_5;
 
-import java.util.Random;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Random;
+
+/**
+ * 服务器端主动推送事件
+ * 只支持新式的浏览器比如chrome和firefox
+ * 实现sse-->server send event 有两点需要实现
+ * （1）服务端表明  produces="text/event-stream"
+ * （2）页面上的js代码，如sse.jsp上
+ */
 @Controller
 public class SseController {
 	
